b1ec5dc74435c70deb9e81e553956653db67995b
Francisco Cervantes Diaz <131913481+Rappu10@users.noreply.github.com>
2024-05-16 07:53:42 -0600

    si

---
+++ Parcial1_2A/3_Entradas_salidas_datos/Entradas_Salidas.py
@@ -0,0 +1,7 @@
+#Solicitar los datos de un alumno
+
+nombre=input("Nombre: ")
+edad=input("Edad: ")
+estatura=input("Estatura: ")
+
+print(f" \t .::: Datos del Alumno :::.. \n Nombre: {nombre} \n Edad: {edad} \n Estatura: {estatura}")
