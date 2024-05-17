c60b6c2e89f844614262849799f377fabfda3292
Francisco Cervantes Diaz <131913481+Rappu10@users.noreply.github.com>
2024-05-17 12:00:43 -0600

    POO_2024

--- Parcial1_2A/6_Ejercicios_Repaso/Ejercicios_Repaso.py
+++ Parcial1_2A/6_Ejercicios_Repaso/Ejercicios_Repaso.py
@@ -1,1 +1,24 @@
-hola
+5
+# Calcular o imprimir el precio a pagar por un artículo
+# El precio a pagar incluye el IVA. El programa deberá funcionar x veces según el deseo del usuario.
+
+
+veces = int(input("Ingrese la cantidad de veces que desea calcular el precio: "))
+
+#
+contador = 1
+
+while contador <= veces:
+
+    precio = float(input("Ingrese el precio del artículo sin IVA: "))
+
+
+    iva = precio * 0.16
+
+   
+    precio_a_pagar = precio + iva
+
+    print(f"El precio a pagar es: {precio_a_pagar}")
+
+
+    contador += 1
