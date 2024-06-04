921351bbf43eedda08378ca2bf515552afea2628
Francisco Cervantes Diaz <131913481+Rappu10@users.noreply.github.com>
2024-06-04 10:21:26 -0600

    POO_2024

---
+++ Parcial1_2A/8_Listas/listas.py
@@ -0,0 +1,13 @@
+"""
+List (Array)
+son colecciones o conjunto de datos/valores bajo 
+un mismo nombre, para acceder a los valores se
+hace con un índice numérico
+
+Nota: sus valores sí son modificables
+
+La lista es una colección ordenada y modificable. permite miembros
+duplicados.
+
+
+"""
